d5573eb40552683d3467410b2b17f5f9b8e59347
Rohan Purekar <rohanpurekar1@gmail.com>
2018-09-09 13:36:15 +0530

    Added basic admin panel

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_jwt import JWT
+from flask_admin import Admin
 from resources.user import UserRegister
 from authentication.security import authenticate, identity
 import os
@@ -10,9 +11,11 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'rohan'
 api = Api(app)
+admin = Admin(app)
 
 
 jwt = JWT(app, authenticate, identity)
+
 @app.route('/')
 def hello():
     return "Bon Voyage with this BoilerPlate"
@@ -22,5 +25,9 @@ api.add_resource(UserRegister,'/register')
 
 if __name__ == '__main__':
     from db import db
+    from flask_admin.contrib.sqla import ModelView
+    from models.user import UserModel
+
     db.init_app(app)
+    admin.add_view(ModelView(UserModel, db.session))
     app.run()
--- run.py
+++ run.py
@@ -1,8 +1,13 @@
-from app import app
+from app import app, admin
 from db import db
+from flask_admin.contrib.sqla import ModelView
+from models.user import UserModel
+
 
 db.init_app(app)
 
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+admin.add_view(ModelView(UserModel, db.session))
